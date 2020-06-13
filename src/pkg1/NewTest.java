29d4ac09f62b5a253370626ef1c709676c2c4d3f
chetanpawar19 <chetanpawar19@outlook.com>
2020-06-14 04:57:07 +0530

    removed chromedriver code. only normal code. Signed-off-by: chetanpawar19 <chetanpawar19@outlook.com>

--- src/pkg1/NewTest.java
+++ src/pkg1/NewTest.java
@@ -4,22 +4,15 @@ import org.testng.annotations.Test;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewTest {
 	public static Logger log = LogManager.getLogger(NewTest.class.getName());;
-	public static WebDriver driver;
 
 	@Test
 	public void f() {
 			log.info("Opening google.co.in");
-			System.setProperty("webdriver.chrome.driver", ".\\usr\\bin\\chromedriver");
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
 			log.info("Window maximized");
-			driver.get("https://www.google.co.in/");
-			log.info("Window title:"+driver.getTitle());
-			log.info("Current URL:"+driver.getCurrentUrl());
+			log.info("Window title:");
+			log.info("Current URL:");
 	}
 }
