b468aa6fe7613ac92cb1b75b8bd7cefe385d0df3
chetanpawar19 <chetanpawar19@outlook.com>
2020-06-13 22:58:20 +0530

    jcommander jar added

--- src/pkg1/NewTest.java
+++ src/pkg1/NewTest.java
@@ -4,14 +4,22 @@ import org.testng.annotations.Test;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewTest {
 	public static Logger log = LogManager.getLogger(NewTest.class.getName());;
+	public static WebDriver driver;
 
 	@Test
 	public void f() {
-		log.info("This is first line");
-		log.info("This is second line");
-		log.info("This is third line");
+			log.info("Opening google.co.in");
+			System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver_linux");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			log.info("Window maximized");
+			driver.get("https://www.google.co.in/");
+			log.info("Window title:"+driver.getTitle());
+			log.info("Current URL:"+driver.getCurrentUrl());
 	}
 }
