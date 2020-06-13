c248cd4bc4816c532ce5c06af247cf740be88bdc
ichetanpawar <66332051+ichetanpawar@users.noreply.github.com>
2020-06-13 23:28:04 +0530

    Update NewTest.java

--- src/pkg1/NewTest.java
+++ src/pkg1/NewTest.java
@@ -14,7 +14,7 @@ public class NewTest {
 	@Test
 	public void f() {
 			log.info("Opening google.co.in");
-			System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver_linux");
+			System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver");
 			driver = new ChromeDriver();
 			driver.manage().window().maximize();
 			log.info("Window maximized");
