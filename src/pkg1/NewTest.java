2110216f39e831f9ea04516d9c25c7448302bce7
ichetanpawar <66332051+ichetanpawar@users.noreply.github.com>
2020-06-14 06:06:29 +0530

    Update NewTest.java

--- src/pkg1/NewTest.java
+++ src/pkg1/NewTest.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class NewTest {
 	public static Logger log = LogManager.getLogger(NewTest.class.getName());;
@@ -15,7 +16,11 @@ public class NewTest {
 	public void f() {
 			log.info("Opening google.co.in");
 			System.setProperty("webdriver.chrome.driver", ".\\usr\\bin\\chromedriver");
-			driver = new ChromeDriver();
+
+			ChromeOptions chromeOptions = new ChromeOptions();
+                	chromeOptions.addArguments("--headless");
+                	chromeOptions.addArguments("--no-sandbox");
+			driver = new ChromeDriver(chromeOptions);
 			driver.manage().window().maximize();
 			log.info("Window maximized");
 			driver.get("https://www.google.co.in/");
