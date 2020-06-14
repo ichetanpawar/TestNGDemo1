6dac714b81fbe6a02222f4510ca37fc662d29989
chetanpawar19 <chetanpawar19@outlook.com>
2020-06-14 11:39:21 +0530

    for headless execution Signed-off-by: chetanpawar19 <chetanpawar19@outlook.com>

--- src/pkg1/NewTest.java
+++ src/pkg1/NewTest.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class NewTest {
 	public static Logger log = LogManager.getLogger(NewTest.class.getName());;
@@ -13,13 +14,19 @@ public class NewTest {
 
 	@Test
 	public void f() {
+			System.setProperty("webdriver.chrome.driver", ".//lib//chromedriver");
+
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--ChromelessOptions");
+            chromeOptions.addArguments("--headless");
+            chromeOptions.addArguments("--no-sandbox");
+			chromeOptions.addArguments("--disable-dev-shm-usage");
+			driver = new ChromeDriver(chromeOptions);
 			log.info("Opening google.co.in");
-			System.setProperty("webdriver.chrome.driver", ".\\usr\\bin\\chromedriver");
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
 			log.info("Window maximized");
 			driver.get("https://www.google.co.in/");
 			log.info("Window title:"+driver.getTitle());
 			log.info("Current URL:"+driver.getCurrentUrl());
+			log.info(driver.getPageSource());
 	}
 }
