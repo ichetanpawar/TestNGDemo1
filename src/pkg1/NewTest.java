16ef87b7504fdcc25bec873d172b648974207e92
ichetanpawar <66332051+ichetanpawar@users.noreply.github.com>
2020-06-19 01:09:42 +0530

    Update NewTest.java

--- src/pkg1/NewTest.java
+++ src/pkg1/NewTest.java
@@ -15,21 +15,21 @@ public class NewTest {
 		log.info("first line");
 			//System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 		log.info("system property set");
-		ChromeOptions chromeOptions = new ChromeOptions();
-			chromeOptions.addArguments("--ChromelessOptions");
-            chromeOptions.addArguments("--headless");
-            chromeOptions.addArguments("--no-sandbox");
-			chromeOptions.addArguments("--disable-dev-shm-usage");
-					chromeOptions.addArguments("--incognito");
-		chromeOptions.setExperimentalOption("useAutomationExtension",false);
-			log.info("options added");
+// 		ChromeOptions chromeOptions = new ChromeOptions();
+// 			chromeOptions.addArguments("--ChromelessOptions");
+//             chromeOptions.addArguments("--headless");
+//             chromeOptions.addArguments("--no-sandbox");
+// 			chromeOptions.addArguments("--disable-dev-shm-usage");
+// 					chromeOptions.addArguments("--incognito");
+// 		chromeOptions.setExperimentalOption("useAutomationExtension",false);
+// 			log.info("options added");
 		//driver = new ChromeDriver(chromeOptions);
-		driver = WebDriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chromeOptions);
+	//	driver = WebDriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chromeOptions);
 		
 		//driver = new RemoteWebDriver(new URL("http://192.168.43.22:4444/wd/hub"),chrome_options=chromeOptions);
 		
 		log.info("before google url");
-		log.info(driver.get("https://www.google.com/"));
+	//	log.info(driver.get("https://www.google.com/"));
 		log.info("last line");
 		
 	}
